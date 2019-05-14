eef995591a5bd8e342aaeb148d132cbb4c9bd8e3
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-05-14 13:47:45 +0100

    Initial commit
    bundler default setup

---
+++ lib/performance/platform/client.rb
@@ -0,0 +1,10 @@
+require "performance/platform/client/version"
+
+module Performance
+  module Platform
+    module Client
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/performance/platform/client/version.rb
@@ -0,0 +1,7 @@
+module Performance
+  module Platform
+    module Client
+      VERSION = "0.1.0"
+    end
+  end
+end
